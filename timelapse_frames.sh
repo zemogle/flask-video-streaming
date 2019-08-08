8c8675eff91e1f3f117a093c57823afe45dedc37
Edward Gomez <edward@gomez.me.uk>
2019-08-08 16:25:10 +0100

    Capture frames for timelapse

---
+++ timelapse_frames.sh
@@ -0,0 +1,5 @@
+wget http://localhost:8000/latest.jpg
+convert latest.jpg -background '#0008' -fill white \
+    -pointsize 36 -font arial caption:$(date +%Y:%m:%dT%H:%M)  \
+-gravity northwest -compose dstout -composite -alpha on latest.jpg
+mv latest.jpg /home/pi/images/image$(date +%Y%m%d%H%M).jpg
