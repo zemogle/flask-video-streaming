bb9d49651198cffe2f2c0680b361f92fd5af5aa6
Edward Gomez <edward@gomez.me.uk>
2019-08-08 16:58:46 +0100

    Make wget quiet

--- timelapse_frames.sh
+++ timelapse_frames.sh
@@ -1,4 +1,4 @@
-wget http://localhost:8000/latest.jpg
+wget -q http://localhost:8000/latest.jpg
 convert latest.jpg -background '#0008' -fill white \
     -pointsize 36 -font arial caption:$(date +%Y:%m:%dT%H:%M)  \
 -gravity northwest -compose dstout -composite -alpha on latest.jpg
